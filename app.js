595ebbe8a074c6b495bec23d9221c64b5024865a
Pavel Milenki <54291798+PavelMilenki@users.noreply.github.com>
2020-02-09 09:51:21 +0300

    add fix for deploy

--- app.js
+++ app.js
@@ -13,13 +13,20 @@ app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
 app.use('/t', require('./routes/redirect.routes'));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+//
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.use(express.static(path.join(__dirname, 'client','build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+});
+
 
 const PORT = process.env.PORT || config.get('port');
 
