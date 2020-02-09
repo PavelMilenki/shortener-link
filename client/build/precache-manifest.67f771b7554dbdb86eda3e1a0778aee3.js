a70e16d8dbb498d5cb191899aaaecbc2f32a8da2
Pavel Milenki <54291798+PavelMilenki@users.noreply.github.com>
2020-02-09 09:46:26 +0300

    add build

---
+++ client/build/precache-manifest.67f771b7554dbdb86eda3e1a0778aee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23169313f6a6951d883b895c01626c83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a72a35c96c3faee75e31",
+    "url": "/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "1ae8eecf397c56a9d1a3",
+    "url": "/static/css/main.dbec4106.chunk.css"
+  },
+  {
+    "revision": "a72a35c96c3faee75e31",
+    "url": "/static/js/2.a510a8f7.chunk.js"
+  },
+  {
+    "revision": "14c9108fd5a38194785d545908fb6cbd",
+    "url": "/static/js/2.a510a8f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ae8eecf397c56a9d1a3",
+    "url": "/static/js/main.6a88d7ed.chunk.js"
+  },
+  {
+    "revision": "25709bcb779763262aae",
+    "url": "/static/js/runtime-main.72b168ec.js"
+  }
+]);
